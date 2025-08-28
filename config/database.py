d8ae2b269f30b01a18995965a7e7e491fbea7fec
Panaers <dyordzcoc@gmail.com>
2025-08-28 16:39:00 +0800

    Added to Models  1. Document Added to route  1. document

--- config/database.py
+++ config/database.py
@@ -1,7 +1,9 @@
 from bson import ObjectId
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://ezrhal:xmqUSDS4FkXAjdGP@imscluster0.9ecljtn.mongodb.net/', 27017)
+#client = MongoClient('mongodb+srv://ezrhal:xmqUSDS4FkXAjdGP@imscluster0.9ecljtn.mongodb.net/', 27017)
+
+client = MongoClient('mongodb://192.168.101.50', 27017)
 
 db = client.DTS
 
