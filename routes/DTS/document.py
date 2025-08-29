672cddfc16aa9c2010fe1c32834f15be442afe05
Panaers <dyordzcoc@gmail.com>
2025-08-29 08:40:44 +0800

    add jsonable_encoder

--- routes/DTS/document.py
+++ routes/DTS/document.py
@@ -2,12 +2,14 @@ from fastapi import APIRouter
 
 from config.database import db
 from models.DTS.document import DocumentModel
+from fastapi.encoders import jsonable_encoder
 from models.RouteToEmployeeModel import PostRouteModel
 from models.DTS.RoutedDocumentModel import StatusHistoryModel
 
 router = APIRouter()
 
 @router.post("/savedocument")
-async def save_document(document: DocumentModel):
-    document = db["Documents"].insert_one(document)
+async def save_document(post_request: DocumentModel):
+    doc = jsonable_encoder(post_request)
+    document = db["Documents"].insert_one(doc)
 
